3f73ee98d5dde2b3c62c7ba59896e8454a4304c6
Eric Maranga <ekirira22@gmail.com>
2016-11-28 08:14:50 +0300

    first webpage on branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+$("#age").click(function(event){
+  event.preventDefault();
+  var age = parseInt($("#age").val());
+    if(age===1){
+      $("#apology").slideToggle(2000);
+    }else {
+      $("#part2").slideToggle(2000);
+  }
+});
+
+$("#sure").click(function(event){
+  event.preventDefault();
+  var sure = parseInt($("#sure").val());
+    if(sure===3){
+      $("#vote").slideToggle(2000);
+    }else{
+      $("#refer").slideToggle(2000);
+  }
+});
+
 $("#show").click(function () {
   $("#candidates").slideDown(2000);
 });
